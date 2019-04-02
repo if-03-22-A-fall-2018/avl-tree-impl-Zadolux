0b8375660f13591b3326b457f3633807f14cc89e
Wallinger Marc <25794895+Zadolux@users.noreply.github.com>
2019-04-02 11:38:25 +0200

    Tests show

---
+++ node.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "node.h"
+
+struct NodeImplementation
+{
+  int key;
+  Node left;
+  Node right;
+  int height;
+};
+
+int get_key(Node node)
+{
+  return 0;
+}
+
+Node get_left(Node node)
+{
+  return 0;
+}
+
+void set_left(Node node, Node left_node)
+{
+
+}
+
+Node get_right(Node node)
+{
+  return 0;
+}
+
+void set_right(Node node, Node right_node)
+{
+
+}
+
+int get_height(Node node)
+{
+  return 0;
+}
+
+void set_height(Node node, int height)
+{
+
+}
+
+Node create_node(int key)
+{
+  return 0;
+}
